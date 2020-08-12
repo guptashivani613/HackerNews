4f97be6c553361a75ebaa81f6007b31f65139716
shivani.gupta <shivani.gupta@kelltontech.com>
2020-08-12 19:09:12 +0530

    changes done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
+  production: false,
   API_URL: 'https://hn.algolia.com/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://hn.algolia.com/api/v1'
 
 };
 
