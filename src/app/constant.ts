138a0c60336334995f297eb4d35488cf4e99a170
shivani.gupta <shivani.gupta@kelltontech.com>
2020-08-12 18:54:35 +0530

    changes done

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,5 +1,6 @@
 import { environment } from '../environments/environment';
 
+//set the endpoint-- currently i have set it here as a static
 export const apiUrl = {
     getItems: `${environment.API_URL}` + '/search?query=foo&tags=story',
 }
